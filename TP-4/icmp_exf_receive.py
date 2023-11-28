bf2a0f26843b60d14ab411010527fb4cda0481ff
ninjawill543 <lucas.hanson@ynov.com>
2023-11-28 11:15:28 +0100

    icmp_exf_receive finished i think

--- TP-4/icmp_exf_receive.py
+++ TP-4/icmp_exf_receive.py
@@ -1,7 +1,10 @@
 from scapy.all import sniff, ICMP
 
 def print_it_please(packet):
-    if (packet['ICMP'].type == 8):
+    if (packet['ICMP'].type == 8 and len(packet['Raw'].load) == 1):
         print(packet['Raw'].load)
+        quit()
 
 sniff(filter="icmp", prn=print_it_please, count=0)
+
+#iface="" if you want to change interface for the sniff
