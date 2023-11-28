1d29ed751e5785f06f9a5d3dae3a876c3d6698b5
ninjawill543 <lucas.hanson@ynov.com>
2023-11-28 19:03:04 +0100

    output string

--- TP-4/icmp_exf_receive_b1.py
+++ TP-4/icmp_exf_receive_b1.py
@@ -2,6 +2,7 @@ from scapy.all import sniff, ICMP
 
 output = ""
 def print_it_please(packet):
+    global output
     if (packet['ICMP'].type == 8 and len(packet['Raw'].load) <= 3):
         if (packet['Raw'].load.decode("utf-8") == "end"):
             print(output)
