e3c89f701567dd8d9687ea71cea96024d6ba2152
ninjawill543 <lucas.hanson@ynov.com>
2023-11-28 10:07:46 +0100

    updated icmp exf receive

--- TP-4/icmp_exf_receive.py
+++ TP-4/icmp_exf_receive.py
@@ -2,10 +2,5 @@ from scapy.all import sniff
 
 def print_it_please(packet):
     print(packet)
-   # print("TCP SYN ACK reçu !")
-   # print(f"- Adresse IP src : {packet['IP'].src}")
-   # print(f"- Adresse IP dst : {packet['IP'].dst}")
-   # print(f"- Port TCP src : {packet['TCP'].sport}")
-   # print(f"- Port TCP dst : {packet['TCP'].dport}")
 
 sniff(filter="icmp", prn=print_it_please, count=0)
