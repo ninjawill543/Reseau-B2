ea4aaea9dcc005a0388945094baedde5d4f2dffc
ninjawill543 <lucas.hanson@ynov.com>
2023-11-28 10:44:00 +0100

    icmp exf receive currently working but not finished

--- TP-4/icmp_exf_receive.py
+++ TP-4/icmp_exf_receive.py
@@ -1,6 +1,7 @@
-from scapy.all import sniff
+from scapy.all import sniff, ICMP
 
 def print_it_please(packet):
-    print(packet)
+    if (packet['ICMP'].type == 0):
+        print(packet['Raw'].load)
 
 sniff(filter="icmp", prn=print_it_please, count=0)
