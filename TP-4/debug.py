5aab6313656e41db4f273ae10fe6700fee2e782e
ninjawill543 <lucas.hanson@ynov.com>
2023-11-29 19:12:59 +0100

    testing

--- TP-4/debug.py
+++ TP-4/debug.py
@@ -11,7 +11,8 @@ def dns_sniff(packet):
         print("Received Domain Name:", domain_name)
         if "~~end~~" in domain_name:
             sorted_packets = ''.join(value for key, value in sorted(received_packets.items()))
-            print("Final Result:", sorted_packets)
+            # Replace '°' with dot when printing the final result
+            print("Final Result:", sorted_packets.replace('°', '.'))
             quit()
         elif "~" in domain_name and not "~~end~~" in domain_name:
             index = domain_name.find("~")
--- TP-4/test_send.py
+++ TP-4/test_send.py
@@ -5,16 +5,13 @@ def dns_send(query_domain, dns_server="lucashanson.tech"):
     dns_request = Ether()/IP(dst=dns_server)/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname=query_domain))
     answers, unanswered_packets = srp(dns_request, timeout=2, verbose=False)
 
-to_send = str(sys.argv[2])
-list = ([to_send[i:i + 6] for i in range(0, len(to_send), 6)])
+to_send = str(sys.argv[2]).replace('.', '°')  # Replace all dots with '°'
+list = [to_send[i:i + 6] for i in range(0, len(to_send), 6)]
 
 count = 0
 for i in list:
-    # Check if the substring ends with a dot
-    if i.endswith('.'):
-        dns_send(str(count) + "~" + i[:-1].replace('.', '°') + "." + sys.argv[1] + "~")  # Replace dot with °
-    else:
-        dns_send(str(count) + "~" + i + "." + sys.argv[1] + "~")
-    count = count + 1
+    dns_send(str(count) + "~" + i + "." + sys.argv[1] + "~")
+    count += 1
+
 dns_send("~~end~~." + sys.argv[1])  # Add . before ~~end~~
 
