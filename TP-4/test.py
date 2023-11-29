efa8137f76c610967a8b1d032641409c300c4374
ninjawill543 <lucas.hanson@ynov.com>
2023-11-29 18:35:51 +0100

    testing

--- TP-4/test.py
+++ TP-4/test.py
@@ -7,13 +7,13 @@ def dns_sniff(packet):
     if DNS in packet and packet.dport == 53 and packet[DNS].qr == 0:
         domain_name = packet[DNSQR].qname.decode('utf-8')
         if "~~end~~" in domain_name:
-            print("Unique strings:", unique_strings)
+            unique_string = ''.join(unique_strings)
+            print(unique_string)
             quit()
         elif "~" in domain_name and not "~~end~~" in domain_name:
             index = domain_name.find("~")
             substring = domain_name[index + 1:-18]
             
-            # Add unique substring to the set
             unique_strings.add(substring)
 
 sniff(filter='udp port 53', prn=dns_sniff)
