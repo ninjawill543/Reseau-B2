358b0f927f987e47b5116f92844e944f1ad58919
ninjawill543 <lucas.hanson@ynov.com>
2023-11-29 18:11:51 +0100

    18 not 19

--- TP-4/DNS_Exfiltration/dns_exfiltration_receive.py
+++ TP-4/DNS_Exfiltration/dns_exfiltration_receive.py
@@ -3,7 +3,7 @@ from scapy.all import *
 def dns_sniff(packet):
     if DNS in packet and packet.dport == 53 and packet[DNS].qr == 0:
         domain_name = packet[DNSQR].qname.decode('utf-8')
-        print(domain_name[:-19])
+        print(domain_name[:-18])
 
 sniff(filter='udp port 53', prn=dns_sniff)
 
