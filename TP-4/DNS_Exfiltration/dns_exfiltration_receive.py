fede2e54a3d5a89820fbc88bbfa7895ce58203e6
ninjawill543 <lucas.hanson@ynov.com>
2023-11-29 18:09:41 +0100

    lets see

--- TP-4/DNS_Exfiltration/dns_exfiltration_receive.py
+++ TP-4/DNS_Exfiltration/dns_exfiltration_receive.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 
 def dns_sniff(packet):
-    if DNS in packet and packet.dport == 53 and packet[DNS].opcode == 0:
+    if DNS in packet and packet.dport == 53 and packet[DNS].qr == 0:
         domain_name = packet[DNSQR].qname.decode('utf-8')
         print(domain_name)
 
