30280a6849405047bdc747251dc4d9a77c04f742
ninjawill543 <lucas.hanson@ynov.com>
2023-11-29 19:07:09 +0100

    testing

--- TP-4/test_send.py
+++ TP-4/test_send.py
@@ -6,15 +6,11 @@ def dns_send(query_domain, dns_server="lucashanson.tech"):
     answers, unanswered_packets = srp(dns_request, timeout=2, verbose=False)
 
 to_send = str(sys.argv[2])
-list = ([to_send[i:i + 6] for i in range(0, len(to_send), 6)])
+list = ([to_send[i:i + 6] + '.' for i in range(0, len(to_send), 6)])
 
 count = 0
 for i in list:
-    # Check if the substring ends with a dot
-    if i.endswith('.'):
-        dns_send(str(count) + "~" + i[:-1].replace('.', '°') + "." + sys.argv[1] + "~")  # Replace dot with °
-    else:
-        dns_send(str(count) + "~" + i + "." + sys.argv[1] + "~")
-    count = count + 1
+    dns_send(str(count) + "~" + i + sys.argv[1] + "~")  # Remove the dot before adding ~
+    count += 1
 dns_send("~~end~~." + sys.argv[1])  # Add . before ~~end~~
 
